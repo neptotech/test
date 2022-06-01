aacdee76f8364270271b0d0655a29259889695db
neptotech <85818947+neptotech@users.noreply.github.com>
2022-06-01 14:28:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let x = 1;
+let switcher = 1;
+
+function delayTimer() {
+  setInterval(delayedFunction, 1000);
+}
+
+function delayedFunction() {
+  if (switcher == 1) {
+    x++;
+  } else {
+    x--;
+  }
+  if (x == 15 || (x == 2 && switcher == -1)) {
+    switcher *= -1;
+  }
+  //console.log(switcher);
+  document.getElementById('container').innerHTML = '<div class="child"></div> '.repeat(x);
+}
+delayTimer();
